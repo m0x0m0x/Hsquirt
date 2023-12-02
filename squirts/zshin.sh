7b29592cbb26a41efa23c2dcf061fd6c30e9b87b
m0x0m0x <123001669+m0x0m0x@users.noreply.github.com>
2023-12-02 09:39:42 +0000

    .

--- squirts/zshin.sh
+++ squirts/zshin.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env sh
 echo " This will install the following "
 echo " 1. zshell - sudo apt intall zsh"
-echo " 2. ohmyzsh - sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)""
+echo " 2. ohmyzsh - sh -c \"\$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)\""
 
 echo ""
