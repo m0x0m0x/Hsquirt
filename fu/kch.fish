221665335d57300df4ce482ab1c1dc08cb9429de
m0x0m0x <123001669+m0x0m0x@users.noreply.github.com>
2023-12-02 08:29:06 +0000

    .

---
+++ fu/kch.fish
@@ -0,0 +1,10 @@
+#!/bin/fish
+/workspaces/Hsquirt/fu/pnp.fish
+/workspaces/Hsquirt/fu/fixrs.fish
+/workspaces/Hsquirt/fu/st.sh
+/workspaces/Hsquirt/fu/aryt.fish
+/workspaces/Hsquirt/fu/tgpt.fish
+/workspaces/Hsquirt/fu/bun.fish
+/workspaces/Hsquirt/fu/tmuxx.fish
+
+
